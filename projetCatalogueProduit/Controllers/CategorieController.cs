5ebe02a2287186f3534ccf8b41514de334eff174
saad <med.saad.mes@gmail.com>
2021-07-10 03:40:57 +0100

    Initial Commit

---
+++ projetCatalogueProduit/Controllers/CategorieController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using projetCatalogueProduit.Models;
+
+namespace projetCatalogueProduit.Controllers
+{
+    public class CategorieController : Controller
+    {
+        CATALOGUE_Entities db = new CATALOGUE_Entities();
+        // GET: Categorie
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult AjoutCategorie()
+        {
+            try
+            {
+                ViewBag.listeCategorie = db.CAT_CATEGORIE.ToList();
+                return View();
+            }
+            catch ( Exception e)
+            {
+                return HttpNotFound();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AjoutCategorie(CAT_CATEGORIE categorie)//enregistrement
+        {
+            try
+            {
+                
+                if (ModelState.IsValid)
+                {
+                    categorie.DATE_SAISIE = DateTime.Now;
+                    db.CAT_CATEGORIE.Add(categorie);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("AjoutCategorie");
+            }
+            catch (Exception e)
+            {
+                return HttpNotFound();
+            }
+        }
+
+        public ActionResult SupprimerCategorie(int id)
+        {
+            try
+            {
+                CAT_CATEGORIE categorie = db.CAT_CATEGORIE.Find(id); //recherchede la categorie
+                if (categorie != null)
+                {
+                    db.CAT_CATEGORIE.Remove(categorie); //supprimer la categorie
+                    db.SaveChanges();//enregistrer le resultat
+                }
+                return RedirectToAction("AjoutCategorie");
+            }
+            catch (Exception e)
+            {
+                return HttpNotFound();
+            }
+        }
+
+        public ActionResult ModifierCategorie(int id)
+        {
+            try
+            {
+                ViewBag.listeCategorie = db.CAT_CATEGORIE.ToList();
+                CAT_CATEGORIE categorie = db.CAT_CATEGORIE.Find(id);
+                if (categorie != null)
+                {
+                    return View("AjoutCategorie",categorie);
+                }
+                return RedirectToAction("AjoutCategorie");
+            }catch(Exception e)
+            {
+                return HttpNotFound();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult ModifierCategorie(CAT_CATEGORIE categorie)
+        {
+            try
+            {
+
+                if (ModelState.IsValid)
+                {
+                    db.Entry(categorie).State = EntityState.Modified; // modification de notre categorie
+                    db.SaveChanges();
+                }
+                return RedirectToAction("AjoutCategorie");
+            }
+            catch (Exception e)
+            {
+                return HttpNotFound();
+            }
+        }
+
+    }
+}
---
+++ projetCatalogueProduit/Controllers/LoginController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using projetCatalogueProduit.Models;
+using System.IO;
+
+namespace projetCatalogueProduit.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Autherize(projetCatalogueProduit.Models.User userModel)
+        {
+            using(CATALOGUE_Entities db = new CATALOGUE_Entities())
+            {
+                var userDetails = db.User.Where(x => x.UserName == userModel.UserName && x.Password == userModel.Password).FirstOrDefault();
+
+                if(userDetails == null)
+                {
+                    userModel.LoginErrorMessage = "Username ou Mot de Passe incorrecte";
+                    return View("Index", userModel);
+                }
+                else
+                {
+                    Session["userID"] = userDetails.UserID;
+                    return RedirectToAction("Index", "Home");
+                }
+
+            }
+            
+        }
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
